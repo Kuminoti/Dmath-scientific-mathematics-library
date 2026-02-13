f291b6eb78fc7613d01d144a8965eb6566d20125
Kuminoti <danielkum02@gmail.com>
2026-02-13 18:59:37 +0100

    added header for datatype classes

---
+++ include/Types/DmathTypes.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef DMATH_TYPES_HPP
+#define DMATH_TYPES_HPP
+
+#include"callbacks.hpp"
+#include"dList.hpp"
+#include"DmathFile.hpp"
+#include"Dstring.hpp"
+#include"logger.hpp"
+#include"node.hpp"
+#include"setTheory.hpp"
+#include"traits.hpp"
+
+#endif
